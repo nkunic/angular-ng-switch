ae43bf8cdb662e00ba59677e973c520b0d0f0418
nkunic <natasa.kunic@gmail.com>
2022-04-04 16:49:37 +0200

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SwitchContentComponent } from './switch-content/switch-content.component';
+import { SwitchItemComponent } from './switch-item/switch-item.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, SwitchContentComponent],
+  declarations: [AppComponent, SwitchContentComponent, SwitchItemComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/switch-item/switch-item.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-item',
+  templateUrl: './switch-item.component.html',
+})
+export class SwitchItemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
